4fe8af6924ace31dde472b6db6afe76aadeb2b58
deborahtandurella <deborah.tandurella01@universitadipavia.it>
2023-06-04 22:31:09 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset=UTF-8>
+      <title>AURORA TANDURELLA | Official Website</title>
+      <link rel="stylesheet" href="style/style.css" />
+      <script src= "script.js"></script>
+   </head>
+   <body>
+      <div id="page-container">
+         <?php include 'menu.php';?>                 
+         <div id="content-wrap">
+            <video id="background-video" autoplay loop muted>
+               <source src="video/bg2.mp4" type="video/mp4">
+            </video>
+         </div>
+         <footer id="footer">&copy; AURORA TANDURELLA 2023</footer>
+      </div>
+   </body>
+</html>
